afe00fc2ff2a4d942f56f8343ae1d0f2bd54a872
Shivam Bandral <shivambandral@gmail.com>
2022-07-02 23:07:51 +0530

    code change bugfix

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,71 +1,67 @@
-import React, {useEffect, useRef} from 'react';
-import CodeMirror from 'codemirror';
-import 'codemirror/theme/dracula.css'; // This is imported for Theme CSS
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/closebrackets';
-import 'codemirror/mode/javascript/javascript'; // To enable the mode below we need to import this.
-import 'codemirror/lib/codemirror.css'; // Editor CSS is imported by this.
+import React, { useEffect, useRef } from "react";
+import Codemirror from "codemirror";
+import "codemirror/theme/dracula.css"; // This is imported for Theme CSS
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/mode/javascript/javascript"; // To enable the mode below we need to import this.
+import "codemirror/lib/codemirror.css"; // Editor CSS is imported by this.
 
-import './Editor.css'
-import ACTIONS from '../action';
-
+import "./Editor.css";
+import ACTIONS from "../action";
 
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
-
   const editorRef = useRef(null);
-  const socketRefValue = socketRef.current;
-
+  
   useEffect(() => {
     async function init() {
-      editorRef.current = CodeMirror.fromTextArea(document.getElementById("realTimeEditor"), {
-        mode: {name: 'javascript', json: true},
-        theme: 'dracula',
-        autoCloseTags: true,
-        autoCloseBrackets: true,
-        lineNumbers: true,
-      })
+      editorRef.current = Codemirror.fromTextArea(
+        document.getElementById("realTimeEditor"),
+        {
+          mode: { name: "javascript", json: true },
+          theme: "dracula",
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
+        }
+      );
 
-      editorRef.current?.on('change', (instance, changes) => {
-        const {origin} = changes;
-
+      editorRef.current.on("change", (instance, changes) => {
+        const { origin } = changes;
+        
         // Getting all the code in code editor
         const code = instance.getValue();
-
+        
         // As soon as someone types in editor, fresh code will be sent to parent component
         onCodeChange(code);
-
-        // 
-        if(origin !== 'setValue'){
-          socketRefValue?.emit(ACTIONS.CODE_CHANGE, { roomId, code});
+        if (origin !== "setValue") {
+          socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+            roomId,
+            code,
+          });
         }
-      })
+      });
     }
-
     init();
-  }, [])
-
+  }, []);
 
   useEffect(() => {
-    const editorRefValue = editorRef.current;
-    if (socketRefValue && editorRefValue) {
-      socketRefValue.on(ACTIONS.CODE_CHANGE, ({ code }) => {
-            if (code !== null) {
-              console.log('socketRef', socketRefValue);
-              editorRefValue.setValue(code);
-            }
-        });
+    if (socketRef.current) {
+      socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+        if (code !== null) {
+          editorRef.current.setValue(code);
+        }
+      });
     }
 
     return () => {
-      socketRefValue?.off(ACTIONS.CODE_CHANGE);
+      socketRef.current.off(ACTIONS.CODE_CHANGE);
     };
-  }, [socketRefValue]);
-  
+  }, [socketRef.current]);
 
   return (
     // Idea is to attach this textarea with CodeMirror so that this will be converted into a Editor
     <textarea id="realTimeEditor" />
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
